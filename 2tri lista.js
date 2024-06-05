2eabaffe78f2ed6ff3e1103262d5f2f1d8271af5
Keta23199 <165030072+Keta23199@users.noreply.github.com>
2024-06-05 09:03:41 -0300

    Create 2tri lista.js

---
+++ 2tri lista.js
@@ -0,0 +1,4 @@
+const produtos=["sabonete","pasta de dente","sabão em pó","água sanitária","amaciante","esponja","álcool"]
+console.log(produtos);
+console.log(produtos[2]);
+console.log(produtos.length);
