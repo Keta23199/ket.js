a007262c5d98e2e40d6fe5570826c113f2f42852
Keta23199 <165030072+Keta23199@users.noreply.github.com>
2024-06-11 10:31:01 -0300

    Update 2tri lista.js

--- 2tri lista.js
+++ 2tri lista.js
@@ -1,4 +1,25 @@
-const produtos=["sabonete","pasta de dente","sabão em pó","água sanitária","amaciante","esponja","álcool"]
-console.log(produtos);
-console.log(produtos[2]);
-console.log(produtos.length);
+const lista = ["Sabão", "Shampoo", "Arroz", "Feijão", "Pão",
+                  "Alface", "Tomate", "Pneu", "Bola", "Banana"]
+
+lista.push("pneu");
+lista.push("linguiseta");                
+console.log(lista);
+console.log(lista[7]);
+console.log(lista.length)
+produtos = []
+
+lista.pop;
+nlista = lista.splice(3,4);
+console.log(nlista);
+lista1 = lista.slice(0,5);
+lista2 = lista.slice(5);
+
+console.log(lista1);
+console.log(lista2);
+
+lista1.push("");
+lista2.push("");
+
+listacompleta = lista1.concat(lista2);
+
+console.log(listacompleta);
